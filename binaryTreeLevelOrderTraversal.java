b46dc088c007d4e27fbd0725a384917b9b172dc4
Mason1993 <zhoumuqing@gmail.com>
2016-01-10 23:51:26 -0600

    Jan.10

---
+++ binaryTreeLevelOrderTraversal.java
@@ -0,0 +1,55 @@
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+// For example:
+// Given binary tree {3,9,20,#,#,15,7},
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+// Tags Tree Breadth-first Search
+// Similar Problems (M) Binary Tree Zigzag Level Order Traversal (E) Binary Tree Level Order Traversal II (E) Minimum Depth of Binary Tree (M) Binary Tree Vertical Order Traversal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        // queue is used to store the next level TreeNode;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();   // to store the subresult, namely the current level node values.
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
