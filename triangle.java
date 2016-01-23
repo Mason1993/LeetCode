088a7840aa9f47a2c1ef3ae80386788c44363ecc
Mason1993 <zhoumuqing@gmail.com>
2016-01-22 23:55:20 -0600

    Jan.22

--- triangle.java
+++ triangle.java
@@ -14,7 +14,47 @@
 
 // Tags Array Dynamic Programming
 
-// wrong solution, greedy algorithm is not applied here. 
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return -1;
+        }
+        if (triangle.get(0) == null || triangle.get(0).size() == 0) {
+            return -1;
+        }
+        // Dynamic Programming, 4 element: state, initialization, function and answer
+        // state to store the each element's result.
+        int n = triangle.size();
+        int[][] distance = new int[n][n]; // distance[row][column]
+        
+        // initialization
+        distance[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            distance[i][0] = distance[i - 1][0] + triangle.get(i).get(0);
+            distance[i][i] = distance[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+        
+        // function
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i; j++) {
+                distance[i][j] = Math.min(distance[i -1][j - 1], distance[i - 1][j]) + triangle.get(i).get(j);
+            }
+        }
+        
+        // answer
+        Arrays.sort(distance[n - 1]);
+        int result = distance[n - 1][0];
+        return result;
+    }
+}
+
+// wrong solution, greedy algorithm is not applied here. consider the following structure:
+// [
+//      [2],
+//     [3,4],
+//    [9,9,10],
+//   [100,100,100,1]
+// ]
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         if (triangle == null || triangle.size() == 0) {
