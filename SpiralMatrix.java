a198767481b7c2e38e04e7a2043188af2d438710
Mason1993 <zhoumuqing@gmail.com>
2016-10-31 00:23:44 -0500

    Oct 31

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -14,3 +14,38 @@
 
 // Hide Tags Array
 // Hide Similar Problems (M) Spiral Matrix II
+
+// Basic idea: keep going while i, j is within row range and column range
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0) {
+            return res;
+        }
+        int rowBegin = 0, rowEnd = matrix.length - 1, columnBegin = 0, columnEnd = matrix[0].length - 1;
+        
+        while (rowBegin <= rowEnd && columnBegin <= columnEnd) { // be attention it allows begin == end
+            for (int j = columnBegin; j <= columnEnd; j++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                res.add(matrix[i][columnEnd]);
+            }
+            columnEnd--;
+            if (rowBegin <= rowEnd) {
+                for (int j = columnEnd; j >= columnBegin; j--) {
+                    res.add(matrix[rowEnd][j]);
+                }
+                rowEnd--;
+            }
+            if (columnBegin <= columnEnd) {
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    res.add(matrix[i][columnBegin]);
+                }
+                columnBegin++;
+            }
+        }
+        return res;
+    }
+}
