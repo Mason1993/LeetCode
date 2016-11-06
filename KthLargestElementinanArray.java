93dea5b82235d22bd2789948cd8208a988287d97
Mason1993 <zhoumuqing@gmail.com>
2016-11-05 23:33:48 -0500

    Nov 5th

---
+++ KthLargestElementinanArray.java
@@ -0,0 +1,27 @@
+// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// For example,
+// Given [3,2,1,5,6,4] and k = 2, return 5.
+
+// Note: 
+// You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+// Credits:
+// Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+// Subscribe to see which companies asked this question
+
+// Hide Tags Heap Divide and Conquer
+// Hide Similar Problems (M) Wiggle Sort II (M) Top K Frequent Elements (E) Third Maximum Number
+
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
