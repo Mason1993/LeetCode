fca5af5efafa6795aa6d8f469e77ca4c974241c8
Mason1993 <zhoumuqing@gmail.com>
2016-11-18 22:50:54 -0600

    Nov 18th

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -22,6 +22,62 @@
 // Hide Tags Depth-first Search Breadth-first Search Graph Topological Sort
 // Hide Similar Problems (M) Course Schedule II (M) Graph Valid Tree (M) Minimum Height Trees
 
+// BFS, much faster, refer to Kahn's algorithm on wiki
+// Kahn's algorithm[edit]
+// One of these algorithms, first described by Kahn (1962), works by choosing vertices in the same order as the eventual topological sort. First, find a list of "start nodes" which have no incoming edges and insert them into a set S; at least one such node must exist in a non-empty acyclic graph. Then:
+
+// L ← Empty list that will contain the sorted elements
+// S ← Set of all nodes with no incoming edges
+// while S is non-empty do
+//     remove a node n from S
+//     add n to tail of L
+//     for each node m with an edge e from n to m do
+//         remove edge e from the graph
+//         if m has no other incoming edges then
+//             insert m into S
+// if graph has edges then
+//     return error (graph has at least one cycle)
+// else 
+//     return L (a topologically sorted order)
+
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        int[] incoming = new int[numCourses];
+        Queue queue = new LinkedList();
+        int count=0;
+        
+        for(int i=0;i<numCourses;i++)
+            graph[i] = new ArrayList();
+            
+        for(int i=0; i<prerequisites.length;i++){
+            incoming[prerequisites[i][0]]++;
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        for(int i=0; i<incoming.length;i++){
+            if(incoming[i] == 0){
+                queue.add(i);
+                count++;
+            }
+        }
+        
+        while(queue.size() != 0){
+            int course = (int)queue.poll();
+            for(int i=0; i<graph[course].size();i++){
+                int pointer = (int)graph[course].get(i);
+                incoming[pointer]--;
+                if(incoming[pointer] == 0){
+                    queue.add(pointer);
+                    count++;
+                }
+            }
+        }
+        if(count == numCourses)
+            return true;
+        else    
+            return false;
+    }
+}
 // DFS
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
